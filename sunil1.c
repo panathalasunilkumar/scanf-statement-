f02de65a1d6ebab604c987cc45b94c3d331ea1c7
panathalasunilkumar <93139903+panathalasunilkumar@users.noreply.github.com>
2021-11-09 11:43:21 +0530

    Add files via upload

---
+++ sunil1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+  void main()
+  {
+  	int a, b,c,d;
+  	printf("Enter 1st number :");
+    scanf("%d",&a);
+	printf("Enter 2nd number :");
+    scanf("%d",&b);
+    printf("Enter 3rd number :");
+    scanf("%d", &c);
+    printf(" number 1: %d\n",a+b);
+    printf(" number2: %d\n",b+c);
+    printf(" number3: %d\n",c+a);
+    
+
+
+  	
+  }
